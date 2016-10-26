4c429ccf7c1eb362b4d50469ab9c507a2dc4fadf
Peter <peterbyrd@yahoo.com>
2016-10-26 10:30:46 -0500

    Added spacing

--- Makefile.R
+++ Makefile.R
@@ -1,9 +1,12 @@
 # Makefile for hypotenuse and angles of a right triangle
+
 setwd("/Users/pbyrd/Git/LiveSession08")
 source ("Hypotenuse Function.R")
 source ("Angles Function.R")
+
 # Test the hypot funciton on a 3-4-5 right triangle
 hypot(3,4)
+
 # Test the ang function on a 3-4-5 right triangle with angles 36.87, 53.13, 90
 ang (3,4)
 
